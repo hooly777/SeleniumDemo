a7cae76dfddd2cce69c11298073dc3e9c5a6a2c2
SIlampurGirish <54929672+SIlampurGirish@users.noreply.github.com>
2020-01-06 14:59:44 +0530

    deleted screenshots
    deleted screenshots

---
+++ src/main/java/com/qa/util/genericutility.java
@@ -0,0 +1,46 @@
+package com.qa.util;
+
+/**
+ * @author Silampur Girish 
+ * @date   06-01-2020
+ * @version 1.o
+ */
+public class genericutility {
+	/**
+	 * @author Silampur Girish
+	 * Method to select dropdown by text
+	 * @param element
+	 * @param text
+	 */
+	public static void selectDropdownByText(WebElement element, String text)
+	{
+		Select select = new Select(element);
+		select.selectByVisibleText(text);
+	}
+
+	/**
+	 * @author Silampur Girish
+	 * Method to select dropdown by Value
+	 * @param element
+	 * @param text
+	 */
+	public static void selectDropdownByValue(WebElement element, String text)
+	{
+		Select select = new Select(element);
+		select.selectByValue(text);
+	}
+	/**
+	 * @author Silampur Girish
+	 * Method to select dropdown by index
+	 * @param element
+	 * @param num
+	 */
+	public static void selectDropdownByIndex(WebElement element, int  num)
+	{
+		Select select = new Select(element);
+		select.selectByIndex(num);
+	}
+
+
+
+}
