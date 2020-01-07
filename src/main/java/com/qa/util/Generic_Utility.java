62538f61fe13f9f43b0a93535f20f9f9ea368e58
sharathgs <sharathgs777@gmail.com>
2020-01-07 12:44:05 +0530

    updated Webgenericutility
    updated Webgenericutility

--- src/main/java/com/qa/util/Generic_Utility.java
+++ src/main/java/com/qa/util/Generic_Utility.java
@@ -1,162 +1,35 @@
 package com.qa.util;
 
+
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-/**
- * @author Silampur Girish 
- * @date   06-01-2020
- * @version 1.o
- */
-public class Webgenericutility {
-	static WebDriver driver;
+public class Generic_Utility {
 
+	 WebDriver driver;
 	/**
-	 * @author Silampur Girish
-	 * Method to select dropdown by text
-	 * @param element
-	 * @param text
+	 * @author Mohini Sethumadhavan
+	 * Method to get browser driver to launch application
+	 * @param WebDriver load
+	 * @param driver
 	 */
-	public static void selectDropdownByText(WebElement element, String text)
+	public WebDriver WebDriverLoad()
 	{
-		Select select = new Select(element);
-		select.selectByVisibleText(text);		
-	}
+		String absolutepath=System.getProperty("user.dir");
+		String filepath=absolutepath+"\\Drivers"+"\\chromedriver.exe";
+		System.setProperty("webdriver.gecko.driver", filepath);
+		ChromeDriver driver=new ChromeDriver();
+		return driver;
 
-	/**
-	 * @author Silampur Girish
-	 * Method to select dropdown by Value
-	 * @param element
-	 * @param text
-	 */
-	public static void selectDropdownByValue(WebElement element, String text)
-	{
-		Select select = new Select(element);
-		select.selectByValue(text);
 	}
 	/**
-	 * @author Silampur Girish
-	 * Method to select dropdown by index
-	 * @param element
-	 * @param num
-	 */
-	public static void selectDropdownByIndex(WebElement element, int  num)
-	{
-		Select select = new Select(element);
-		select.selectByIndex(num);
-	}
-	/**
-	 * @author Silampur Girish
-	 * Method to get attribute value of the element
-	 * @param element
-	 * @param attribute
-	 * @return String
-	 */
-	public static String getAttributeValue(WebElement element, String attribute)
-	{
-		return element.getAttribute(attribute);
-	}
-
-	/**
-	 * @author Silampur Girish
-	 * Method to get SelectedText value of the dropdwon
-	 * @param element	 
-	 * @return String
-	 */
-	public static String getSelectedText(WebElement element)
-	{
-		Select select = new Select(element);
-		return select.getFirstSelectedOption().getText();
-	}
-	/**
-	 * @author Uma Maheswari
-	 * Method to Navigate url 
-	 * @param url
-	 */
-	public static void NavigateToUrl(String url)
-	{
-		driver.navigate().to(url);
-	}
-
-	/**
-	 * @author Lakshmi
-	 * Method to take you back by one page on the browser’s history.
-	 */ 
-	public static void Navigateback()
-	{
-		driver.navigate().back();
-	}
-	/**
-	 * @author Uma Maheswari
-	 * Method to take you forward by one page on the browser’s history.
-	 */ 
-
-	public static void NavigateForward()
-	{
-		driver.navigate().forward();
-	}
-
-	/**
-	 * @author Uma Maheswari
-	 * Method to refresh the page
-	 */ 
-
-	public static void NavigateRefresh()
-	{
-		driver.navigate().refresh();
-	}
-
-	/**
-	 * @author Uma Maheswari
-	 * Method to get title of the application/page
-	 * return String
-	 */ 
-
-	public static String getTitle()
-	{
-		return driver.getTitle();
-	}
-
-	/**
-	 * @author Uma Maheswari
-	 * Method to click an Element
-	 * @param element
-	 */
-
-	// Method to Click Element
-	public static  void ClickElement(WebElement element)
-	{
-		element.click();
-	}
-
-	/**
-	 * @author Lakshmi
-	 * Method to get text of the element
-	 * @param element
-	 */
-
-	public static String getText(WebElement element)
-	{
-		return element.getText();
-	}
-
-	/**
-	 * @author Uma Maheswari
-	 * Method to send text
-	 * @param element
-	 * @param Text
-	 */
-	public static void sendText(WebElement element, String Text)
-	{
-		element.sendKeys(Text);
-	}
-	//
-		/**
 	 * @author Mohini Sethumadhavan
 	 * Method to select any element for boolean value
 	 * @param WebElement 
@@ -236,7 +109,7 @@ public class Webgenericutility {
 	{
 		driver.manage().timeouts().pageLoadTimeout(i, TimeUnit.SECONDS);
 		return null;
-
+		
 	}
 	/**
 	 * @author Mohini Sethumadhavan
@@ -247,7 +120,7 @@ public class Webgenericutility {
 	public WebElement waitExpectedConditions(WebElement waitExpectedException, int seconds)
 	{
 		WebDriverWait wait=new WebDriverWait(driver,seconds);
-
+		
 		wait.until(ExpectedConditions.elementToBeSelected(waitExpectedException));
 		return waitExpectedException;
 
@@ -258,22 +131,22 @@ public class Webgenericutility {
 	 * @param WebElement 
 	 * @param waitExpectedException
 	 */
-
+	
 	public WebElement waitForElement(WebElement elementWait, int seconds) {
 
-		WebDriverWait wait = new WebDriverWait(driver, seconds);
-		wait.until(ExpectedConditions.elementToBeClickable(elementWait));
-		return elementWait;
-	}
+		 WebDriverWait wait = new WebDriverWait(driver, seconds);
+		 wait.until(ExpectedConditions.elementToBeClickable(elementWait));
+		 return elementWait;
+		}
 	/**
 	 * @author Mohini Sethumadhavan
 	 * Method to check an element to be invisible
 	 * @param WebElement 
 	 * @param waitExpectedException
 	 */
-
+	
 	public WebElement waitTillElementFound(WebElement ElementTobeFound,
-			int seconds) 
+	int seconds) 
 	{
 		WebDriverWait wait = new WebDriverWait(driver, seconds);
 		wait.until(ExpectedConditions.visibilityOf(ElementTobeFound));
@@ -286,7 +159,7 @@ public class Webgenericutility {
 	 * @param waitExpectedException
 	 */
 	public WebElement waitframeToBeAvailableAndSwitchToIt(WebElement frameLocator,
-			int seconds) 
+	int seconds) 
 	{
 		WebDriverWait wait = new WebDriverWait(driver, seconds);
 		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(frameLocator));
@@ -302,7 +175,7 @@ public class Webgenericutility {
 	public WebElement switchToParentWindow() {
 		driver.switchTo().defaultContent();
 		return null;
-	}
+		}
 	/**
 	 * @author Mohini Sethumadhavan
 	 * Method to switch frame 
@@ -310,11 +183,12 @@ public class Webgenericutility {
 	 * @param 
 	 */	
 	public WebElement switchToFrame(WebElement iframeElement) {
-		driver.switchTo().frame(iframeElement);
-		return iframeElement;
+	driver.switchTo().frame(iframeElement);
+	return iframeElement;
 	}
-
-
-
-
+	
+	
 }
+
+
+
