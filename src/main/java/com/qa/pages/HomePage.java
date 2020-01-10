7faac408b8d0cb9eaf1e1821b16a5bbe25b51541
Rajesh <rajesh@gmail.com>
2020-01-10 20:23:10 +0530

    Update HomePage.java

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-	
 import com.qa.Utility.Webgenericutility;
 
 public class HomePage {
